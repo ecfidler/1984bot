5c8557ef57c2504eeaf5f8728d0fb6e0415ff3c0
Ethan Fidler <ethancf1999@gmail.com>
2022-04-01 08:15:44 -0400

    i

--- helpers/roleUpdateHelper.js
+++ helpers/roleUpdateHelper.js
@@ -38,10 +38,12 @@ async function updateRoles(message) {
 
     updateMemberTierRoles(extraMembers, [midRole, highRole], lowRole);
 
-    await message.guild.channels.fetch(GENERAL_ID, { force: true })
-    let announcementChannel = message.guild.channels.cache.get(GENERAL_ID);
-    console.log(channel);
-    channel.send({ embeds: [announcementEmbed()]});
+    await message.guild.channels.fetch(GENERAL_ID, { force: true }).then( channel => { // 674689826976694276 GENERAL_ID
+        console.log(channel);
+        channel.send({ embeds: [announcementEmbed()]});
+    }).catch( err => {
+        console.log(err);
+    });
 };
 
 function updateMemberTierRoles(members, rolesToRemove, roleToAdd) {
